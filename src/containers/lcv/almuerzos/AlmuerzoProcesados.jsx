e5bdda0f065d57b51c268de4f72bdcad84b666f6
emunoz-rwittmer <edison@tiptoptravel.ec>
2023-10-09 22:55:08 -0500

    upgrades

--- src/containers/lcv/almuerzos/AlmuerzoProcesados.jsx
+++ src/containers/lcv/almuerzos/AlmuerzoProcesados.jsx
@@ -109,7 +109,7 @@ const AlmuerzoProcesados = () => {
                                             }
                                             <td>
                                                 <select onChange={(e) => handleChange(e.target.value, refrigerio.cedulaCliente)} className="file-input-sm file-input-info outline-none input-bordered focus:outline-none focus:ring-1  w-[120px] rounded-md shadow-base-300 shadow-lg">
-                                                    <option value={selected}>Seleccione</option>
+                                                    <option value="">Seleccione</option>
                                                     {servicesState.services.map((service) => (
                                                         <option key={service.id} value={service.id}>{service.name}</option>
                                                     ))}
