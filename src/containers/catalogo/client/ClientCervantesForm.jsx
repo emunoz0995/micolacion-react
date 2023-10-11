c974c848cd27ea15dd744c31ee95b0c4563ed227
emunoz-rwittmer <edison@tiptoptravel.ec>
2023-10-11 10:06:52 -0500

    upgrade

--- src/containers/catalogo/client/ClientCervantesForm.jsx
+++ src/containers/catalogo/client/ClientCervantesForm.jsx
@@ -33,7 +33,7 @@ const ClientCervantesForm = () => {
         toast: true,
         position: 'top-end',
         showConfirmButton: false,
-        timer: 2000,
+        timer: 1000,
         timerProgressBar: true,
         didOpen: (toast) => {
             toast.addEventListener('mouseenter', Swal.stopTimer)
