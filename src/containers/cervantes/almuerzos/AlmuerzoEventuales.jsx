5ced165e04d8806edd652ca3eee2d668fba8ba07
emunoz-rwittmer <edison@tiptoptravel.ec>
2023-10-25 15:39:41 -0500

    agregando extras en almuerzos de cervantes

--- src/containers/cervantes/almuerzos/AlmuerzoEventuales.jsx
+++ src/containers/cervantes/almuerzos/AlmuerzoEventuales.jsx
@@ -7,21 +7,39 @@ import SchoolLayout from '../../../layouts/SchoolsLayout';
 import TabParts from '../../../components/breadcrumbs/TabParts';
 import MainLoader from '../../../components/Loaders/MainLoader';
 import BtnTable from '../../../components/buttons/BtnTable';
+import Swal from 'sweetalert2';
+
 //SLICES
 import { setIsLoading } from '../../../store/slices/isLoading.slice';
+import { getServicesExtrasThunk } from '../../../store/slices/catalogs/services.slice';
+import { registerExtrasThunk } from '../../../store/slices/procedures/funtions.slice';
 import { incrementLunchThunk } from '../../../store/slices/procedures/almuerzos.slice';
 
 const AlmuerzoEventualesCervantes = () => {
     const { school_id } = useParams();
     const isLoading = useSelector(state => state.isLoadingSlice);
+    const servicesState = useSelector(state => state.services);
     const dispatch = useDispatch();
     const [hiddenRows, setHiddenRows] = useState([]);
     const [data, setData] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
 
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+
     useEffect(() => {
         getAlmuerzosEventuales();
+        dispatch(getServicesExtrasThunk());
     }, []);
 
     useEffect(() => {
@@ -56,6 +74,24 @@ const AlmuerzoEventualesCervantes = () => {
         hideRow(id);
     }
 
+    const handleChange = (serviceId, cedulaCliente) => {
+        const data = {
+            serviceId,
+            cedulaCliente,
+        }
+
+        dispatch(registerExtrasThunk(data));
+
+        Toast.fire({
+            icon: 'success',
+            title: '¡Servicio extra registrado!'
+        }).then(function (result) {
+            getAlmuerzosEventuales();
+        })
+
+    };
+
+
     const getAlmuerzosEventuales = () => {
         dispatch(setIsLoading(true));
         axios.get(`https://system.micolacion.com/api/almuerzos_cervantes/lunch_eventuales/${school_id}`)
@@ -75,45 +111,54 @@ const AlmuerzoEventualesCervantes = () => {
             ) : (
                 <div className="mx-5 my-5 w-full">
                     <TabParts
-                      titleOne={'Primaria'} toOne={`/schools/${school_id}/almuerzos_primaria`} activeOne={false}
-                      titleTwo={'Inicial'} toTwo={`/schools/${school_id}/almuerzos_inicial`} activeTwo={false}
-                      titleTree={'Secundaria '} toTree={`/schools/${school_id}/almuerzos_secundaria`} activeTree={false}
-                      titleFour={'Eventuales'} toFour={`/schools/${school_id}/almuerzos_eventuales_cervantes`} activeFour={true}
-                      titleSix={'Procesados'} toSix={`/schools/${school_id}/almuerzos_procesados_cervantes`} activeSix={false}
+                        titleOne={'Primaria'} toOne={`/schools/${school_id}/almuerzos_primaria`} activeOne={false}
+                        titleTwo={'Inicial'} toTwo={`/schools/${school_id}/almuerzos_inicial`} activeTwo={false}
+                        titleTree={'Secundaria '} toTree={`/schools/${school_id}/almuerzos_secundaria`} activeTree={false}
+                        titleFour={'Eventuales'} toFour={`/schools/${school_id}/almuerzos_eventuales_cervantes`} activeFour={true}
+                        titleSix={'Procesados'} toSix={`/schools/${school_id}/almuerzos_procesados_cervantes`} activeSix={false}
                     />
                     <div className="overflow-y-scroll h-[87%] contenedor">
                         <table className="text-[13px] table table-zebra w-full">
                             <thead className='sticky top-0 border-t-2 border-t-sky-500' >
                                 <tr>
-                                    <th className='w-[300px]'>Nombre</th>
+                                    <th>Nombre</th>
                                     <th className='flex justify-center'>Almuerzo</th>
                                     <th>Total</th>
-                                    <th>Servicio</th>
+                                    <th>Extra</th>
                                     <th>Nivel</th>
+                                    <th>Servicio</th>
                                 </tr>
                             </thead>
                             {searchResults.length > 0 ?
-                            <tbody>
-                                {searchResults.map(almuerzo => {
-                                    if (hiddenRows.includes(almuerzo.id)) {
-                                        return null; 
-                                    }
-                                    return (
-                                        <tr className="uppercase" key={almuerzo.id}>
-                                            <td>{almuerzo.lastName} {almuerzo.firstName} </td>
-                                            <td className='flex justify-center'> <BtnTable action="decrement" funtion={() => handlePlusBreak(almuerzo.cedulaCliente,almuerzo.id)} /></td>
-                                            <td>{almuerzo.lunchesConsumed}</td>
-                                            <td>{almuerzo.cliente_servicio?.name}</td>
-                                            <td>{almuerzo.cliente_seccion?.name}</td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                            :
-                            <div className="absolute z-10 top-[200px] left-3 sm:left-[380px]">
-                                <h1 className='font-semibolt text-[22px] sm:text-[25px] text-gray-400'>NO HAY DATOS PARA MOSTRAR</h1>
-                            </div>
-                        }
+                                <tbody>
+                                    {searchResults.map(almuerzo => {
+                                        if (hiddenRows.includes(almuerzo.id)) {
+                                            return null;
+                                        }
+                                        return (
+                                            <tr className="uppercase" key={almuerzo.id}>
+                                                <td>{almuerzo.lastName} {almuerzo.firstName} </td>
+                                                <td className='flex justify-center'> <BtnTable action="decrement" funtion={() => handlePlusBreak(almuerzo.cedulaCliente, almuerzo.id)} /></td>
+                                                <td>{almuerzo.totalBreakfast}</td>
+                                                <td>
+                                                    <select onChange={(e) => handleChange(e.target.value, almuerzo.cedulaCliente)} className="file-input-sm file-input-info outline-none input-bordered focus:outline-none focus:ring-1  w-[120px] rounded-md shadow-base-300 shadow-lg">
+                                                        <option value="">Seleccione</option>
+                                                        {servicesState.services.map((service) => (
+                                                            <option key={service.id} value={service.id}>{service.name}</option>
+                                                        ))}
+                                                    </select>
+                                                </td>
+                                                <td>{almuerzo.cliente_seccion?.name}</td>
+                                                <td>{almuerzo.cliente_servicio?.name}</td>
+                                            </tr>
+                                        );
+                                    })}
+                                </tbody>
+                                :
+                                <div className="absolute z-10 top-[200px] left-3 sm:left-[380px]">
+                                    <h1 className='font-semibolt text-[22px] sm:text-[25px] text-gray-400'>NO HAY DATOS PARA MOSTRAR</h1>
+                                </div>
+                            }
                         </table>
                     </div>
                 </div>
--- src/containers/cervantes/almuerzos/AlmuerzoInicial.jsx
+++ src/containers/cervantes/almuerzos/AlmuerzoInicial.jsx
@@ -7,21 +7,38 @@ import SchoolLayout from '../../../layouts/SchoolsLayout';
 import TabParts from '../../../components/breadcrumbs/TabParts';
 import MainLoader from '../../../components/Loaders/MainLoader';
 import BtnTable from '../../../components/buttons/BtnTable';
+import Swal from 'sweetalert2';
 //SLICES
 import { setIsLoading } from '../../../store/slices/isLoading.slice';
+import { getServicesExtrasThunk } from '../../../store/slices/catalogs/services.slice';
+import { registerExtrasThunk } from '../../../store/slices/procedures/funtions.slice';
 import { decrementLunchThunk } from '../../../store/slices/procedures/almuerzos.slice';
 
 const AlmuerzoInicial = () => {
     const { school_id } = useParams();
     const isLoading = useSelector(state => state.isLoadingSlice);
+    const servicesState = useSelector(state => state.services);
     const dispatch = useDispatch();
     const [hiddenRows, setHiddenRows] = useState([]);
     const [data, setData] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
 
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+
     useEffect(() => {
         getAlmuerzosInicial();
+        dispatch(getServicesExtrasThunk());
     }, []);
 
     useEffect(() => {
@@ -56,6 +73,23 @@ const AlmuerzoInicial = () => {
         hideRow(id);
     }
 
+    const handleChange = (serviceId, cedulaCliente) => {
+        const data = {
+            serviceId,
+            cedulaCliente,
+        }
+
+        dispatch(registerExtrasThunk(data));
+
+        Toast.fire({
+            icon: 'success',
+            title: '¡Servicio extra registrado!'
+        }).then(function (result) {
+            getAlmuerzosInicial();
+        })
+
+    };
+
     const getAlmuerzosInicial = () => {
         dispatch(setIsLoading(true));
         axios.get(`https://system.micolacion.com/api/almuerzos_cervantes/lunch_inicial/${school_id}`)
@@ -85,30 +119,39 @@ const AlmuerzoInicial = () => {
                         <table className="text-[13px] table table-zebra w-full">
                             <thead className='sticky top-0 border-t-2 border-t-sky-500' >
                                 <tr>
-                                    <th className='w-[300px]'>Nombre</th>
+                                    <th>Nombre</th>
                                     <th className='flex justify-center'>Almuerzo</th>
                                     <th>Total</th>
-                                    <th>Servicio</th>
+                                    <th>Extra</th>
                                     <th>Nivel</th>
+                                    <th>Servicio</th>
                                 </tr>
                             </thead>
                             {searchResults.length > 0 ?
-                            <tbody>
-                                {searchResults.map(almuerzo => {
-                                    if (hiddenRows.includes(almuerzo.id)) {
-                                        return null;
-                                    }
-                                    return (
-                                        <tr className="uppercase" key={almuerzo.id}>
-                                            <td>{almuerzo.lastName} {almuerzo.firstName} </td>
-                                            <td className='flex justify-center'> <BtnTable action="decrement" funtion={() => handlePlusBreak(almuerzo.cedulaCliente, almuerzo.id)} /></td>
-                                            <td>{almuerzo.totalLunch}</td>
-                                            <td>{almuerzo.cliente_servicio?.name}</td>
-                                            <td>{almuerzo.cliente_seccion?.name}</td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody> :
+                                <tbody>
+                                    {searchResults.map(almuerzo => {
+                                        if (hiddenRows.includes(almuerzo.id)) {
+                                            return null;
+                                        }
+                                        return (
+                                            <tr className="uppercase" key={almuerzo.id}>
+                                                <td>{almuerzo.lastName} {almuerzo.firstName} </td>
+                                                <td className='flex justify-center'> <BtnTable action="decrement" funtion={() => handlePlusBreak(almuerzo.cedulaCliente, almuerzo.id)} /></td>
+                                                <td>{almuerzo.totalBreakfast}</td>
+                                                <td>
+                                                    <select onChange={(e) => handleChange(e.target.value, almuerzo.cedulaCliente)} className="file-input-sm file-input-info outline-none input-bordered focus:outline-none focus:ring-1  w-[120px] rounded-md shadow-base-300 shadow-lg">
+                                                        <option value="">Seleccione</option>
+                                                        {servicesState.services.map((service) => (
+                                                            <option key={service.id} value={service.id}>{service.name}</option>
+                                                        ))}
+                                                    </select>
+                                                </td>
+                                                <td>{almuerzo.cliente_seccion?.name}</td>
+                                                <td>{almuerzo.cliente_servicio?.name}</td>
+                                            </tr>
+                                        );
+                                    })}
+                                </tbody> :
                                 <div className="absolute z-10 top-[200px] left-3 sm:left-[380px]">
                                     <h1 className='font-semibolt text-[22px] sm:text-[25px] text-gray-400'>NO HAY DATOS PARA MOSTRAR</h1>
                                 </div>
