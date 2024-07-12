e935bd529605537d6a1fb8f61976b0a215870c18
emunoz-rwittmer <131011264+emunoz-rwittmer@users.noreply.github.com>
2024-07-12 12:01:24 -0500

    arreglo

--- src/store/constans.js
+++ src/store/constans.js
@@ -1,2 +1,2 @@
-//export const API_BASE_URL = "https://system.micolacion.com/";
-export const API_BASE_URL = "http://localhost:3000/";
+export const API_BASE_URL = "https://www.system.micolacion.com/";
+//export const API_BASE_URL = "http://localhost:3000/";
