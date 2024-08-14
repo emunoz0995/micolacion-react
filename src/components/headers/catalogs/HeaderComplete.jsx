617fc6d4b008510c090948b20779a80b3abdf35f
emunoz-rwittmer <131011264+emunoz-rwittmer@users.noreply.github.com>
2024-08-14 16:43:33 -0500

    upgrades

--- src/components/headers/catalogs/HeaderComplete.jsx
+++ src/components/headers/catalogs/HeaderComplete.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import BtnAdd from '../../buttons/BtnAdd';
-import { FaSearch } from 'react-icons/fa';
 
-
-const HeaderComplete = ({ title, to, placeholder, register,  onclick }) => {
+const HeaderComplete = ({ title, to, placeholder, value, onChange }) => {
 
     return (
-        <div className='w-ful flex text-2xl font-semibold justify-between mb-5'>
+        <div className='w-ful flex text-2xl font-semibold justify-between mb-5 items-center'>
             <div className='flex gap-2'>
                 <h1>{title}</h1>
                 <BtnAdd to={to} />
             </div>
             <div className='flex items-center ' >
-                <input className="outline-none input-bordered focus:outline-none focus:ring-1  text-sm border font-normal p-1 rounded-l-lg" type="text" placeholder={placeholder} {...register} />
-                <button
-                    className="bg-sky-400  hover:bg-sky-600 text-white transition-all active:scale-95 p-2 rounded-r-lg font-bold shadow-lg shadow-base-content/30 flex items-center gap-1 justify-center text-sm "
-                    type="submit"
-                    onClick={onclick}
-                >
-                    <FaSearch />
-                </button>
+                <input
+                    value={value}
+                    onChange={onChange}
+                    placeholder={placeholder}
+                    className="outline-none input-bordered focus:outline-none focus:ring-1  text-sm border font-normal p-1 rounded-lg w-[300px]" type="text"  />
             </div>
 
         </div>
--- src/containers/catalogo/service/ServiceList.jsx
+++ src/containers/catalogo/service/ServiceList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from "react-i18next";
 import { useNavigate } from 'react-router-dom';
@@ -12,19 +12,20 @@ import IconStatus from '../../../components/icons/IconStatus';
 import Toast from '../../../utils/toast';
 //SLICE
 import { getServicesThunk, deleteServiceThunk } from '../../../store/slices/catalogs/services.slice';
+import HeaderComplete from '../../../components/headers/catalogs/HeaderComplete';
 
 const ServiceList = () => {
 
     const { t } = useTranslation();
+    const [searchTerm, setSearchTerm] = useState('');
     const serviceState = useSelector(state => state.services);
     const navigate = useNavigate();
     const dispatch = useDispatch();
- 
+
 
     useEffect(() => {
         dispatch(getServicesThunk());
     }, []);
-
 
     if (serviceState.message.message === "resource deleted successfully") {
         dispatch(getServicesThunk());
@@ -33,6 +34,16 @@ const ServiceList = () => {
     const handleDelete = (yard_id) => {
         dispatch(deleteServiceThunk(yard_id));
     };
+
+    const handleSearch = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const filteredItinerary = searchTerm
+        ? serviceState.services.filter(service =>
+            service.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        : serviceState.services;
 
     if (serviceState.error.error === "invalid token") {
         Toast.fire({
@@ -43,47 +54,53 @@ const ServiceList = () => {
             navigate("/login");
         }, 2000);
     }
-    console.log(serviceState)
+
     return (
         <HomeLayout>
-             {serviceState.fetching || serviceState.processing ? (
+            {serviceState.fetching || serviceState.processing ? (
                 <MainLoader />
             ) : (
                 <div className="mx-5 my-5 w-full">
-                <HeaderSimple title='Servicios' to='/services_new' />
+                    <HeaderComplete
+                        title='Servicios'
+                        to='/services_new'
+                        placeholder={"Buscar ..."}
+                        value={searchTerm}
+                        onChange={handleSearch}
+                    />
                     <div className="overflow-y-scroll h-[87%] contenedor">
                         <table className="text-[13px] table table-zebra w-full">
                             <thead className='border-t-2 border-t-sky-500' >
                                 <tr>
                                     <th className='w-[15px]'></th>
-                                    <th>Nombre</th> 
-                                    <th>Codigo</th> 
+                                    <th>Nombre</th>
+                                    <th>Codigo</th>
                                     <th>Precio</th>
                                     <th>Colegio</th>
-                                    <th>Tipo</th>    
+                                    <th>Tipo</th>
                                     <th></th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {serviceState.services.map(service => (
+                                {filteredItinerary.map(service => (
                                     <tr key={service.id}>
                                         <td><IconStatus active={service.active} /></td>
-                                        <td>{service.name}</td>  
-                                        <td>{service.code}</td>  
+                                        <td>{service.name}</td>
+                                        <td>{service.code}</td>
                                         <td>{service.price}</td>
                                         <td>
                                             {
-                                            service.isCervantes && service.isLcv ? "Cervantes - Liceo Campo Verde":
-                                            service.isCervantes ? "Cervantes" :
-                                            service.isLcv ? "Liceo Campo Verde" :
-                                            ""
+                                                service.isCervantes && service.isLcv ? "Cervantes - Liceo Campo Verde" :
+                                                    service.isCervantes ? "Cervantes" :
+                                                        service.isLcv ? "Liceo Campo Verde" :
+                                                            ""
                                             }
                                         </td>
                                         <td>{
                                             service.isExtra ? "Extra" :
-                                            service.isAditional ? "Adicional" :
-                                            "Principal"
-                                            }</td>                        
+                                                service.isAditional ? "Adicional" :
+                                                    "Principal"
+                                        }</td>
                                         <td className='flex gap-1 justify-end'>
                                             <BtnTable action="edit" to={`/services/${service.id}`} />
                                             <BtnTable title="¿Quieres eliminar este servicio?" action="delete" onclick={() => handleDelete(service.id)} />
